b521486f3d118e5f8a099754b7654e3371c2f69d
ThinHubbins <fredrickchinweze30@gmail.com>
2026-01-22 01:03:46 +0100

    Done with hero section and carousel

--- sitcity/src/pages/Home.js
+++ sitcity/src/pages/Home.js
@@ -1,15 +1,103 @@
-import React from "react";
+import React, {useRef} from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import chair from "../assets/img/chairMain.png";
 import arrow from "../assets/img/contactarr.svg";
 import couch1 from "../assets/img/couch.png";
 import couch2 from "../assets/img/couch2.png";
 import couch3 from "../assets/img/couch3.png";
 import centercouch from "../assets/img/centerchair.png";
+import centerhero1 from "../assets/img/centerhero1.png";
+import centerhero2 from "../assets/img/centerhero2.png";
+import forsale1 from "../assets/img/forsale1.jpg";
+import forsale2 from "../assets/img/forsale2.jpg";
+import forsale3 from "../assets/img/forsale3.jpg";
+import left from "../assets/img/left.svg";
+
 
 const Home = () => {
+  const sliderRef = useRef(null)
+
+  const slides = [
+  {
+    image: forsale1,
+    name: "Green Beyani Chair",
+    address: "Montego Heights",
+    price: "450",
+  },
+  {
+    image: forsale2,
+    name: "Blue Beyani Chair",
+    address: "Santa Anna",
+    price: "750",
+  },
+  {
+    image: forsale3,
+    name: "Bour Couple Chair",
+    address: "Amani Crested",
+    price: "1,200",
+  },
+   {
+    image: forsale1,
+    name: "Green Beyani Chair",
+    address: "Montego Heights",
+    price: "450",
+  },
+  {
+    image: forsale2,
+    name: "Blue Beyani Chair",
+    address: "Santa Anna",
+    price: "750",
+  },
+  {
+    image: forsale3,
+    name: "Bour Couple Chair",
+    address: "Amani Crested",
+    price: "1,200",
+  },
+];
+
+ var settings = {
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3.5,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+
   return (
-    <div className="container mt-4">
-      <div className="row">
+    
+    <div className="container-fluid mt-4">
+      <div className="row hero">
         <div className="col-6">
           <nav className="navbar">
             <div className="container-fluid d-flex justify-content-around">
@@ -92,11 +180,14 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="row align-items-center">
-        <div className="col-md-6">
-          <div className="container">
-            <img className="centerchair" src={centercouch} alt="centercouch" style={{ maxWidth: "100%", height: "auto" }} />
-          </div>
+      <div className="row">
+        <div className="col-12 col-xl-6">
+          <img
+            className="centerchair"
+            src={centercouch}
+            alt="centercouch"
+            style={{ maxWidth: "100%", height: "auto" }}
+          />
         </div>
         <div className="col-md-6 text-white d-flex align-items-center">
           At SitCity, we meticulously handcraft each piece using only the finest
@@ -105,6 +196,110 @@ const Home = () => {
           and modern innovation, ensuring that every chair not only looks
           stunning but also provides an unparalleled seating experience.
         </div>
+      </div>
+      <div className=" container my-5">
+        <div className="row justify-content-center align-items-center centerhero">
+          <div className="col-6 d-flex justify-content-end">
+            <img
+              src={centerhero1}
+              alt="centerhero1"
+              style={{ maxWidth: "392px" }}
+            />
+          </div>
+          <div className="col-6 d-flex justify-content-start">
+            <img
+              src={centerhero2}
+              alt="centerhero2"
+              style={{ maxWidth: "392px" }}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="container-fluid p-0">
+        <div className="row g-0 mt-5">
+          <div className="col-6 text-white d-flex justify-content-start carousel">
+            <div className="carousel-header text-start">
+              <span
+                style={{
+                  fontWeight: "273",
+                  fontSize: "24px",
+                }}
+              >
+                DISCOVER SIT CITY
+              </span>
+              <p>
+                EXPLORE OUR <br /> LUXURY CHAIRS
+              </p>
+            </div>
+          </div>
+          <div className="col-6 d-flex justify-content-end align-items-end">
+            <div
+              className="text-center view"
+              style={{
+                width: "146.33px",
+                height: "44.32px",
+                background: "#FFFFFF",
+                padding: "10px 20px",
+                borderRadius: "40px",
+              }}
+            >
+              View More
+            </div>
+          </div>
+        </div>
+      </div>
+        <Slider ref={sliderRef} {...settings}>
+  {slides.map((slide, i) => (
+    <div
+      className="px-3 mt-4 d-flex justify-content-center position-relative"
+      key={i}
+      style={{ minHeight: '300px' }} 
+    >
+      <img
+        className="img-fluid rounded chairCarousel"
+        src={slide.image}
+        alt={`forsale${i}`}
+      />
+
+    
+      <div
+        className="position-absolute"
+        style={{
+          bottom: '3%',
+          left: '50%',
+          transform: 'translateX(-50%)',
+          background: 'rgba(255, 255, 255, 0.24)',
+          boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+          backdropFilter: 'blur(4px)',
+          borderRadius: '12px',
+          padding: '0.5rem 1rem',
+          width: '82%',
+          height: '45px',
+        }}
+      >
+        <span className="fw-bold text-dark">{slide.title} by {slide.author}</span>
+      </div>
+    </div>
+  ))}
+</Slider>
+
+     <div className="arrow-row row my-4">
+      <div className="col-6 d-flex justify-content-end">
+        <div className="" onClick={() => sliderRef.current.slickPrev()}>
+           <div className="border rounded" style={{
+            width: '57px', height: '46px', background: 'white', cursor: 'pointer', display: 'flex', justifyContent: 'center', alignItems: 'center'
+          }}>
+          <img src={left} alt="left arrow" />
+        </div></div></div>
+        <div className="col-6 d-flex justify-content-start">
+        <div onClick={() => sliderRef.current.slickNext()}>
+          <div className="border rounded" style={{
+            width: '57px', height: '46px', background: 'white', cursor: 'pointer', display: 'flex', justifyContent: 'center', alignItems: 'center'
+          }}>
+          <img src={left} alt="right arrow" style={{
+            rotate: '180deg'
+          }} />
+        </div></div></div>
       </div>
     </div>
   );
